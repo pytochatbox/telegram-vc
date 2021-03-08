6b3d640f0929641a4b373362195c2ecb74f2ea4d
pytochatbox <77884261+pytochatbox@users.noreply.github.com>
2021-03-08 19:05:55 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+api_id = 2048409
+api_hash = "649e419e5fa739da02b649780d492c1b"
+radio_link = "http://peridot.streamguys.com:7150/Mirchi"
+sudo_chat_id = -1001235672935
+owner_id = 911646244
--- sample_config.py
+++
@@ -1,5 +0,0 @@
-api_id = 14371
-api_hash = "e46b6c854d2bf58a0"
-radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485876964
-owner_id = 1243703097
